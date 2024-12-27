133e661d03bc67ecbbf286ff3ae97fbe970d117d
saaadk7 <hashmiisaad@gmail.com>
2024-12-27 17:25:54 +0530

    all the Ios Issues resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist', // This must match Capacitor's webDir
+    sourcemap: true, // Optional: Helps debug errors in production
+  },
+  server: {
+    host: true,
+    port: 3000,
+  },
+});
+
+// we have to reconfigure it fully . 
+// aslo have to made this Ios Directory 
+// also we've to reconfigure the Xcode 
+// we need to reBuild for better performance and OutPut .
